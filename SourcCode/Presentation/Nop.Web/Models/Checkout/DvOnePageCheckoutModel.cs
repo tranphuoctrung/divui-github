ff8db030e38823329c27741578a27ae4b3b5a709
tranphuoctrung <tranphuoctrung1989@gmail.com>
2016-04-19 23:36:36 +0700

    Onepagecheckout

--- SourcCode/Presentation/Nop.Web/Models/Checkout/DvOnePageCheckoutModel.cs
+++ SourcCode/Presentation/Nop.Web/Models/Checkout/DvOnePageCheckoutModel.cs
@@ -1,4 +1,5 @@
 ﻿using Nop.Web.Framework.Mvc;
+using Nop.Web.Models.Divui.ShoppingCart;
 using Nop.Web.Models.ShoppingCart;
 using System;
 using System.Collections.Generic;
@@ -13,16 +14,30 @@ namespace Nop.Web.Models.Checkout
         public OnePageCheckoutModel()
         {
             DiscountBox = new DiscountBoxModel();
+            CheckoutPaymentMethod = new CheckoutPaymentMethodModel();
+            CheckoutConfirm = new CheckoutConfirmModel();
+            BillingAddress = new CheckoutBillingAddressModel();
+            Pickups = new List<PickupModel>();
         }
         public OrderTotalsModel OrderTotals { get; set; }
 
         public CheckoutBillingAddressModel BillingAddress { get; set; }
+
+        //phương thức thanh toán
+        public CheckoutPaymentMethodModel CheckoutPaymentMethod { get; set; }
+
+        public string SelectedPaymentMethodSystem { get; set; }
 
         public DiscountBoxModel DiscountBox { get; set; }
 
         public bool TermsOfServiceOnShoppingCartPage { get; set; }
 
         public bool CouponCollapsed { get; set; }
+
+        //thông tin giỏ hàng
+        public CheckoutConfirmModel CheckoutConfirm { get; set; }
+
+        public List<PickupModel> Pickups { get; set; }
 
         public partial class DiscountBoxModel : BaseNopModel
         {
---
+++ SourcCode/Presentation/Nop.Web/Models/Divui/ShoppingCart/PickupModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nop.Web.Models.Divui.ShoppingCart
+{
+    public class PickupModel
+    {
+        public string Value { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int ProductOptionId { get; set; }
+
+        public int ProductAttributeId { get; set; }
+
+        public int AttributeControlTypeId { get; set; }
+    }
+}
